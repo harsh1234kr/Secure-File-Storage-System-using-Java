4d9a4e4860e06d19be531bd0cf1a569c1d09f5f7
Harsh Kumar <85002498+harsh1234kr@users.noreply.github.com>
2023-10-04 23:34:08 +0530

    Update user_authentication.java

--- user_authentication.java
+++ user_authentication.java
@@ -52,6 +52,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .formLogin();
     }
 }
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, authenticated user!";
+    }
+}
+
 
 
 
