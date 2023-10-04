d13cb5a7076f7996b83ae4c5c342b21d71e6fdb4
Harsh Kumar <85002498+harsh1234kr@users.noreply.github.com>
2023-10-04 23:32:01 +0530

    Update user_authentication.java

--- user_authentication.java
+++ user_authentication.java
@@ -1,1 +1,11 @@
+@Entity
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    // getters, setters, and other methods
+}
 
