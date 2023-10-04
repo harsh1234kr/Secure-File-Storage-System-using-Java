82e6c0be9c17e29559ebc79112f60badad058705
Harsh Kumar <85002498+harsh1234kr@users.noreply.github.com>
2023-10-04 23:32:59 +0530

    Update user_authentication.java

--- user_authentication.java
+++ user_authentication.java
@@ -25,5 +25,33 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         return new User(appUser.getUsername(), appUser.getPassword(), new ArrayList<>());
     }
 }
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/login", "/register").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin();
+    }
+}
+
 
 
