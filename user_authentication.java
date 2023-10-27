80ca83567a37c43ccc8fec46486bf603acbaa236
Harsh Kumar <85002498+harsh1234kr@users.noreply.github.com>
2023-10-27 22:50:43 +0530

    Update user_authentication.java

--- user_authentication.java
+++ user_authentication.java
@@ -1,12 +1,4 @@
-@Entity
-public class AppUser {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    private String username;
-    private String password;
-    // getters, setters, and other methods
 }
 public interface UserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByUsername(String username);
