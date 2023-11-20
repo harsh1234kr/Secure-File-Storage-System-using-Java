491deaa4759051a5b6f4b0777e43f492b127ff04
Harsh Kumar <85002498+harsh1234kr@users.noreply.github.com>
2023-11-20 19:40:43 +0530

    Update user_authentication.java

--- user_authentication.java
+++ user_authentication.java
@@ -44,7 +44,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
    
   
   
+ @Override
+      protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/login", "/register").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin();
+    }
 
+}
 
 
 
